c5772d14f2268b2c74797a14aa8149e6f57771f5
Eduardo Freitas <eduardoflorencio96@gmail.com>
2022-07-14 00:35:06 -0300

    add special characteres

--- backend/src/main/java/com/devsuperior/dsmeta/service/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SmsService.java
@@ -30,10 +30,11 @@ public class SmsService {
     public void sendSms(Long saleId) {
         Sale sale = saleRepository.findById(saleId).get();
 
-        String date = sale.getDate().getMonthValue() + " " + sale.getDate().getYear();
+        String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+        String amount = String.format("%.2f", sale.getAmount());
 
         String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-                + " com um total de R " + String.format("%.0f", sale.getAmount());
+                + " com um total de R$ " + amount;
 
         Twilio.init(twilioSid, twilioKey);
 
